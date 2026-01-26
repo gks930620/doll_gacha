2271045dc70d2f16fa7020e588b0d54ae9b6df5a
gks930620 <gks930620@naver.com>
2026-01-26 10:43:45 +0900

    옿ㄹ

--- src/main/java/com/doll/gacha/HomeController.java
+++ src/main/java/com/doll/gacha/HomeController.java
@@ -1,8 +1,6 @@
 package com.doll.gacha;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
